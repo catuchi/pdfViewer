b911781853af13f66e36dd3cf26720070c39bf3b
Chibu <atuchibueze@gmail.com>
2022-08-16 11:19:18 -0400

    implement renderPage function

--- js/main.js
+++ js/main.js
@@ -10,12 +10,39 @@ const scale = 1.5,
   ctx = canvas.getContext("2d");
 
 // Render the page
-function renderPage(num) {}
+function renderPage(num) {
+  pageIsRendering = true;
+
+  // Get page
+  pdfDoc.getPage(num).then((page) => {
+    // Set scale
+    const viewport = page.getViewport({ scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderContext = {
+      canvasContext: ctx,
+      viewport,
+    };
+
+    page.render(renderContext).promise.then(() => {
+      pageIsRendering = false;
+
+      if (pageNumIsPending !== null) {
+        renderPage(pageNumIsPending);
+        pageNumIsPending = null;
+      }
+    });
+
+    // Output current page
+    document.getElementById("page-num").textContent = num;
+  });
+}
 
 // Get Document
 pdfjsLib.getDocument(url).promise.then((pdfDoc_) => {
   pdfDoc = pdfDoc_;
-  console.log(pdfDoc);
+  // console.log(pdfDoc);
 
   document.querySelector("#page-count").textContent = pdfDoc.numPages;
 
